33dea4cca5b62d5111878d97ca847610b518fc36
Alvaro Gomez <alvagomuri@gmail.com>
2024-11-28 12:17:03 +0100

    Salva pantallas

--- JavaScript/Movimiento/app.js
+++ JavaScript/Movimiento/app.js
@@ -1,12 +1,89 @@
+//recogida de sprite
 const sprite=document.getElementById("sprite");
-const X=sprite.style.marginLeft;
-const Y=sprite.style.margintop;
 
-document.getElementById("iniciar").addEventListener("click",()=>{
-    setInterval(()=>{
-        sprite.style.display="flex";
-        X+=5+"px";
-        Y+=5+"px";
-    },50)
+//movimiento
+let X = 0;
+let Y = 0;
+
+//codiciones rebotes
+let condicionX = false;
+let condicionY = false;
+
+//borde de ventana
+ventanaX = window.innerWidth - 50;
+ventanaY = window.innerHeight - 50;
+
+window.addEventListener("resize",()=>{
+    ventanaX = window.innerWidth - 50;
+    ventanaY = window.innerHeight - 50;
 })
+
+//evita la aceleracion
+let intervalo;
+
+//control de tiempo al pulsar 
+window.addEventListener("load",()=>{
+    if (!intervalo){
+        intervalo = setInterval(mover,25)
+    }
+})
+
+//funcion para mover
+function mover(){
+    if (condicionX == false && condicionY == false) {
+        X += 5;
+        Y += 5;
+
+        sprite.style.marginLeft = X + "px";
+        sprite.style.marginTop = Y + "px";
+
+        borde();
+    }
+    if (condicionX == true && condicionY == false) {
+        X -= 5;
+        Y += 5;
+
+        sprite.style.marginLeft = X + "px";
+        sprite.style.marginTop = Y + "px";
+
+        borde();
+    }
+
+    if(condicionX == false && condicionY == true) {
+        X += 5;
+        Y -= 5;
+
+        sprite.style.marginLeft = X + "px";
+        sprite.style.marginTop = Y + "px";
+
+        borde();
+    }
+
+    if (condicionX == true && condicionY == true) {
+        X -= 5;
+        Y -= 5;
+
+        sprite.style.marginLeft = X + "px";
+        sprite.style.marginTop = Y + "px";
+
+        borde();
+    }
+}  
+
+//funcion detectar borde de ventana
+function borde(){
+    if (ventanaX <= X){
+        condicionX = true;
+    }
+    if (0 >= X){
+        condicionX = false;
+    } 
+
+    if (ventanaY <= Y){
+        condicionY = true;
+    }
+    if (0 >= Y){
+        condicionY = false;
+    }
+}
 
