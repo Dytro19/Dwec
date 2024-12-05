d1b07dfa7748b44857e3fae257f70ac3b80ccbbf
Alvaro Gomez <alvagomuri@gmail.com>
2024-12-05 12:43:27 +0100

    clases y tp

---
+++ JavaScript/Pruebas JavaScript/POO/app.js
@@ -0,0 +1,16 @@
+class Personajes{
+    constructor(n, e){
+        this.nombre=n;
+        this.edad=e;
+    }
+
+}
+
+let mario=new Personajes("mario", 30);
+let luigi=new Personajes("luigi", 32);
+let yoshi=new Personajes("yoshi", 200);
+
+console.log(mario);
+console.log(luigi);
+console.log(yoshi);
+
