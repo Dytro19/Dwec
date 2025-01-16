5561e96b94b4ea02c66d77c3719fdc8233880885
Alvaro Gomez <alvagomuri@gmail.com>
2025-01-16 12:50:18 +0100

    assbdjdasdh

---
+++ JavaScript/Pruebas JavaScript/LocalStorage/app.js
@@ -0,0 +1,9 @@
+localStorage.setItem('nombre', 'Juan');
+localStorage.setItem('apellido', 'Perez');
+localStorage.setItem('edad', 30);
+
+console.log(localStorage.getItem('nombre'));
+console.log(localStorage.getItem('apellido'));
+console.log(localStorage.getItem('edad'));
+
+localStorage.removeItem('edad');
